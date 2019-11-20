1b390e9e8a988bdac17ec049754e3b00fa964389
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-11-20 13:09:55 -0800

    implement dark mode and vector assets

--- weather_dog/Controller/WeatherViewController.swift
+++ weather_dog/Controller/WeatherViewController.swift
@@ -6,4 +6,20 @@
 //  Copyright © 2019 Jessica Trinh. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+class WeatherViewController: UIViewController {
+    @IBOutlet weak var conditionImageView: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var cityLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
