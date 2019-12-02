d629e16d828cf07de4b7993c5cf873ef047bd6ee
Jessica Trinh <jtjessicatrinh@gmail.com>
2019-12-02 11:30:20 -0800

    create reminder view controllers

--- weather_dog/Controller/WeatherViewController.swift
+++ weather_dog/Controller/WeatherViewController.swift
@@ -8,18 +8,28 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
+    // access delegate when return button is pressed by user 
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        print(searchTextField.text!)
+        return true
+    }
 
 }
