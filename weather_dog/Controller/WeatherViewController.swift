b53b78333255da3b8f91b4d0315c4f5c0c891512
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-07 14:00:41 -0800

    edit textfield for valid inputs

--- weather_dog/Controller/WeatherViewController.swift
+++ weather_dog/Controller/WeatherViewController.swift
@@ -15,7 +15,7 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
-    
+        
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -29,13 +29,13 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func searchPressed(_ sender: UIButton) {
-        print(searchTextField.text!)
+        searchTextField.endEditing(true)
     }
     
     // access delegate when return button is pressed by user 
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchTextField.endEditing(true)
-        print(searchTextField.text!)
+//        print(searchTextField.text!)
         return true
     }
     
@@ -54,10 +54,6 @@ class WeatherViewController: UIViewController, UITextFieldDelegate {
         // use searchTextField.text to get the weather for that city 
         searchTextField.text = ""
     }
-    
-    @IBAction func checkBoxTapped(_ sender: UIButton) {
-    }
-    
     
 
 }
