a61a584b9af04c58b30686144b03979c9ac157e8
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-07 10:21:36 -0800

    implement Reminder functionality

--- weather_dog/Controller/Reminder.swift
+++ weather_dog/Controller/Reminder.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+import UIKit
+
+class Reminder: NSObject {
+
+    var notification: UNNotificationRequest
+    var name: String
+    var time: NSDate
+    
+    // Archive paths for Persistent Data when saving a reminder
+    static let DocumentsDirectory = FileManager().urls(for: .documentDirectory, in: .userDomainMask).first!
+    static let ArchiveURL = DocumentsDirectory.appendingPathComponent("reminders")
+    
+    // enum for property keys
+    enum PropertyKey {
+        case name, time, notification
+    }
+    
+    init(name: String, time: NSDate, notification: UNNotificationRequest){
+        self.name = name
+        self.time = time
+        self.notification = notification
+        
+        super.init()
+    }
+
+}
