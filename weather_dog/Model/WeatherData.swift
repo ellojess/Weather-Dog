aa2d4c73e71b0ed6c467f94e9bd69211b376a482
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-08 01:24:54 -0800

    create structure for weather data

--- weather_dog/Model/WeatherData.swift
+++ weather_dog/Model/WeatherData.swift
@@ -6,4 +6,19 @@
 //  Copyright © 2020 Jessica Trinh. All rights reserved.
 //
 
+// Data obtained from Dark Sky Api 
+
 import Foundation
+
+struct WeatherData: Decodable {
+    let name: String
+    let currently: Currently
+}
+
+struct Currently: Decodable {
+    let temperature: Double
+    let summary: String
+    let icon: String
+}
+
+
