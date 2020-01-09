7929b9243f2a8a2873911c84d8aa33198c4e47c3
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-08 17:06:48 -0800

    edit privacy for CoreLocation/GeoLocation

--- weather_dog/Model/LocationManager.swift
+++ weather_dog/Model/LocationManager.swift
@@ -6,4 +6,38 @@
 //  Copyright © 2020 Jessica Trinh. All rights reserved.
 //
 
-import Foundation
+// This file converts geo location to city name
+
+//import Foundation
+//import CoreLocation
+//
+//class LocationManager: NSObject {
+//    
+//    private let locationManager = CLLocationManager()
+//    
+//    override init() {
+//        super.init()
+//        self.locationManager.delegate = (self as CLLocationManagerDelegate)
+//        self.locationManager.desiredAccuracy = kCLLocationAccuracyBest
+//        self.locationManager.requestWhenInUseAuthorization()
+//    }
+//}
+//
+//// MARK: - Core Location Delegate
+//extension LocationManager: CLLocationManagerDelegate {
+//    
+//    
+//    func locationManager(_ manager: CLLocationManager,
+//                         didChangeAuthorization status: CLAuthorizationStatus) {
+//
+//        switch status {
+//    
+//        case .notDetermined         : print("notDetermined")        // location permission not asked for yet
+//        case .authorizedWhenInUse   : print("authorizedWhenInUse")  // location authorized
+//        case .authorizedAlways      : print("authorizedAlways")     // location authorized
+//        case .restricted            : print("restricted")           // TODO: handle
+//        case .denied                : print("denied")               // TODO: handle
+//        }
+//    }
+//}
+
