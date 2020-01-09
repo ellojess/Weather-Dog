637c2942d399bfaba6b19a782874abb0df33fd05
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-09 00:54:54 -0800

    imeplement Open Weather API

--- weather_dog/Model/Constants.swift
+++ weather_dog/Model/Constants.swift
@@ -7,3 +7,15 @@
 //
 
 import Foundation
+
+struct Constants {
+    // Dark Sky
+    static let apiKey = "93c9ccf24d15846ad9482aa3a59728d0"
+    // "5f48b2583b60968733bba398d1c1b3a8" moi
+    // request format: https://api.darksky.net/forecast/[key]/[latitude],[longitude]
+    
+    // Open Weather
+    static let apiKey1 = "8719bc329041b7b1b49ddad7f3bd7959"
+    // request format: https://api.openweathermap.org/data/2.5/weather?q={city name}
+    // https://api.openweathermap.org/data/2.5/weather?appid={key}&q={city name}&units=imperial
+}
