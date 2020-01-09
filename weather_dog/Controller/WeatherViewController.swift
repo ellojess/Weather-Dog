318e2a246d2ab56258df8d1e0cb4be71f70f56b4
Jessica Trinh <jtjessicatrinh@gmail.com>
2020-01-08 17:26:14 -0800

    CoreLocation, CLLocationManagerDelegate edit, bug fix

--- weather_dog/Controller/WeatherViewController.swift
+++ weather_dog/Controller/WeatherViewController.swift
@@ -17,80 +17,6 @@ class WeatherViewController: UIViewController {
     @IBOutlet weak var timeLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
     @IBOutlet weak var searchTextField: UITextField!
-        
-//    var weatherManager = WeatherManager()
-//    let locationManager = CLLocationManager()
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        locationManager.delegate = self as? CLLocationManagerDelegate
-//        locationManager.requestWhenInUseAuthorization()
-//        locationManager.requestLocation()
-//
-//        weatherManager.delegate = self // set current class as delegate so that weatherManager.delegate property != nil
-//        searchTextField.delegate = self
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose any resources that can be recreated
-//    }
-//
-//    @IBAction func searchPressed(_ sender: UIButton) {
-//        searchTextField.endEditing(true)
-//    }
-//
-//    // access delegate when return button is pressed by user
-//    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
-//        searchTextField.endEditing(true)
-////        print(searchTextField.text!)
-//        return true
-//    }
-//
-//    // validate what user is typing, make sure it's not an empty string
-//    func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
-//        if textField.text != ""{
-//            return true
-//        } else {
-//            textField.placeholder = "Type something here"
-//            return false
-//        }
-//    }
-//
-//    // delegate notified when textfield is done editing
-//    func textFieldDidEndEditing(_ textField: UITextField) {
-//
-//        if let city = searchTextField.text {
-//            weatherManager.fetchWeather(cityName: city)
-//        }
-//
-//        // use searchTextField.text to get the weather for that city
-//        searchTextField.text = ""
-//    }
-//
-//    func didUpdateWeather(_ weatherManager: WeatherManager, weather: WeatherModel) {
-//        // print(weather.temperature)
-//
-//        DispatchQueue.main.async {
-//            // Update temperature label in UI
-//            self.temperatureLabel.text = weather.temperatureString
-//            // Update icon image in UI
-//            self.conditionImageView.image = UIImage(systemName: weather.conditionName)
-//            // Update city name label in UI
-//            self.cityLabel.text = weather.cityName
-//        }
-//
-//    }
-//
-//    func didFailWithError(error: Error){
-//        print(error)
-//    }
-//
-//
-//extension Weather
-//
-//}
 
     var weatherManager = WeatherManager()
     let locationManager = CLLocationManager()
@@ -116,11 +42,13 @@ extension WeatherViewController: UITextFieldDelegate {
         searchTextField.endEditing(true)
     }
     
+    // access delegate when return button is pressed by user
     func textFieldShouldReturn(_ textField: UITextField) -> Bool {
         searchTextField.endEditing(true)
         return true
     }
     
+    // validate what user is typing, make sure it's not an empty string
     func textFieldShouldEndEditing(_ textField: UITextField) -> Bool {
         if textField.text != "" {
             return true
